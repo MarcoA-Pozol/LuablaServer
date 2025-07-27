2f11198d08f82c0402691794ebe9627cbf8fa056
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-07-26 21:18:58 -0600

    New data in DB

--- Social/views.py
+++ Social/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUNDs, HTTP_500_INTERNAL_SERVER_ERROR
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from . models import Notification
@@ -19,7 +19,7 @@ class NotificationsView(APIView):
 
             notifications_list = Notification.objects.filter(destinatary=user).all()
             if len(notifications_list) < 1:
-                return Response({'error': 'No notifications were found'}, status=HTTP_404_NOT_FOUNDs)
+                return Response({'error': 'No notifications were found'}, status=HTTP_404_NOT_FOUND)
 
             serialied_notifications_list = NotificationSerializer(notifications_list, many=True)
             return Response({'notifications':serialied_notifications_list}, status=HTTP_200_OK)
