e42c182445d2f2a819e5697d770d516163cc2f11
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-07 12:34:02 -0600

    Added pagination to list_posts_by_language view

--- Hub/urls.py
+++ Hub/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/all', views.list_posts, name='list_all_posts'),
-    path('posts', views.list_posts_by_user_id, name='list_posts'),
-    path('post', views.create_post, name='create_post'),
+    path('posts/all', views.list_posts_by_language, name='list_all_posts'),
+    path('posts', views.list_posts_by_user, name='list_posts'),
+    path('post', views.PostView.as_view(), name='post'),
 ]
--- Hub/views.py
+++ Hub/views.py
@@ -2,23 +2,37 @@ from . models import Post
 from . serializers import PostResponseSerializer, PostCreateUpdateSerializer, PostCommentResponseSerializer, PostCommentCreateUpdateSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def list_posts_by_language(request):
+    """Get all posts by language. All posts from other users."""
+    try:
+        language = request.data.get('language')
+        
+        posts = Post.objects.filter(language=language).order_by('-created_at')
+
+        paginator = PageNumberPagination()
+        paginator.page_size = 10 
+
+        paginated_posts = paginator.paginate_queryset(posts, request)
+
+        serializer = PostResponseSerializer(paginated_posts, many=True)
+        
+        posts = serializer.data
+
+        return Response({'items':posts}, status=HTTP_200_OK)
+    except Exception as e:
+        return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def list_posts(request):
-    language = request.data.get('language')
-    
-    posts = Post.objects.filter(language=language).all()
-    serialized_posts = PostResponseSerializer(posts, many=True)
-
-    return Response({'items':serialized_posts}, status=HTTP_200_OK)
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def list_posts_by_user_id(request):
+def list_posts_by_user(request):
+    """Get all posts of a language from the auth user."""
     language = request.data.get('language')
     user = request.user
     
