c6d33391f371cf22dc146ca10d4c538c884420ac
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-06 22:59:20 -0600

    Added language field to Post model

--- Hub/models.py
+++ Hub/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from Authentication.models import User
 from . validators import validate_audio_file, validate_image_file
 from Luabla.models import BaseModel
+from Decks.datasets import LANGUAGE_CHOICES
+from Flashcards.datasets import INDO_EUROPEAN_LANGUAGES
+
+LANGUAGES = LANGUAGE_CHOICES.extend(INDO_EUROPEAN_LANGUAGES)
 
 class Post(BaseModel):
+    language = models.CharField(max_length=3, choices=LANGUAGES, default='EN', null=False, blank=True, db_index=True)
     title = models.CharField(max_length=150, null=False)
     author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE, db_index=True, null=False)
     opinion = models.TextField(blank=True, null=True)
