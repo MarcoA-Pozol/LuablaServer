ebeb90f94aa5d0af6e1c63403e790d7b6a9a1b10
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-07 12:01:56 -0600

    Created endpoints to create, get by id, and delete post

--- Hub/urls.py
+++ Hub/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts', views.list_posts, name='list_posts'),
-    path('create_post', views.create_post, name='create_post'),
+    path('posts/all', views.list_posts, name='list_all_posts'),
+    path('posts', views.list_posts_by_user_id, name='list_posts'),
+    path('post', views.create_post, name='create_post'),
 ]
--- Hub/views.py
+++ Hub/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -26,17 +27,41 @@ def list_posts_by_user_id(request):
     return Response({'items':posts}, status=HTTP_200_OK)
     
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def create_post(request):
-    serializer = PostCreateUpdateSerializer(data=request.data)
-
-    if serializer.is_valid():
-        new_item = serializer.save(author=request.user)
-
-        serialized_item = PostResponseSerializer(new_item)
-        item = serialized_item.data
-        
-        return Response({'item':item}, status=HTTP_201_CREATED)
+class PostView(APIView):
+    permission_classes = [IsAuthenticated]
     
-    return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    def get(self, request):
+        """Get post by post id"""
+        try:
+            post_id = request.query_params.get('id')
+            
+            post = PostResponseSerializer(Post.objects.get(id=post_id))
+            
+            return Response({'item':post}, status=HTTP_200_OK)
+        except Exception as e:
+            return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def post(self, request):
+        """Create post by request body"""
+        serializer = PostCreateUpdateSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_item = serializer.save(author=request.user)
+
+            serialized_item = PostResponseSerializer(new_item)
+            item = serialized_item.data
+            
+            return Response({'item':item}, status=HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        try:
+            post_id = request.query_params.get('id')
+            
+            post = Post.objects.get(id=post_id)
+            post.delete()
+            
+            return Response({'item':post}, status=HTTP_200_OK)
+        except Exception as e:
+            return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
