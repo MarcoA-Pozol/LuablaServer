872fc78465050c53b5488d85e8e864feea2c10b6
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-07 13:05:10 -0600

    Added mixin for managing exceptions in APIViews

--- Hub/views.py
+++ Hub/views.py
@@ -1,13 +1,13 @@
 from . models import Post
 from . serializers import PostResponseSerializer, PostCreateUpdateSerializer, PostCommentResponseSerializer, PostCommentCreateUpdateSerializer
 from rest_framework.decorators import api_view, permission_classes, throttle_classes
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from . throttles import ListPostsByLanguageThrottle
 from Luabla.decorators import manage_exceptions
+from Luabla.mixins import ExceptionHandlerAPIView
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated, IsAdminUser])
@@ -43,20 +43,17 @@ def list_posts_by_user(request):
     return Response({'items':posts}, status=HTTP_200_OK)
     
 
-class PostView(APIView):
+class PostView(ExceptionHandlerAPIView):
     permission_classes = [IsAuthenticated]
     
     def get(self, request):
         """Get post by post id"""
-        try:
-            post_id = request.query_params.get('id')
-            
-            post = PostResponseSerializer(Post.objects.get(id=post_id))
-            
-            return Response({'item':post}, status=HTTP_200_OK)
-        except Exception as e:
-            return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
-    
+        post_id = request.query_params.get('id')
+        
+        post = PostResponseSerializer(Post.objects.get(id=post_id))
+        
+        return Response({'item':post}, status=HTTP_200_OK)
+
     def post(self, request):
         """Create post by request body"""
         serializer = PostCreateUpdateSerializer(data=request.data)
@@ -73,65 +70,55 @@ class PostView(APIView):
     
     def put(self, request):
         """Update post values completely"""
+        post_id = request.query_params.get('id')
+        if not post_id:
+            return Response({'error': 'Missing id'}, status=400)
 
         try:
-            post_id = request.query_params.get('id')
-            if not post_id:
-                return Response({'error': 'Missing id'}, status=400)
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found'}, status=404)
 
-            try:
-                post = Post.objects.get(id=post_id)
-            except Post.DoesNotExist:
-                return Response({'error': 'Post not found'}, status=404)
+        serializer = PostCreateUpdateSerializer(
+            post,
+            data=request.data, 
+            partial=False 
+        )
 
-            serializer = PostCreateUpdateSerializer(
-                post,
-                data=request.data, 
-                partial=False 
-            )
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'item': serializer.data}, status=200)
+        
+        return Response(serializer.errors, status=400)
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'item': serializer.data}, status=200)
-            
-            return Response(serializer.errors, status=400)
-        except Exception as e:
-            return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
-    
     def patch(self, request):
         """Update post's values partially"""
+        post_id = request.query_params.get('id')
+        if not post_id:
+            return Response({'error': 'Missing id'}, status=400)
+
         try:
-            post_id = request.query_params.get('id')
-            if not post_id:
-                return Response({'error': 'Missing id'}, status=400)
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found'}, status=404)
 
-            try:
-                post = Post.objects.get(id=post_id)
-            except Post.DoesNotExist:
-                return Response({'error': 'Post not found'}, status=404)
+        serializer = PostCreateUpdateSerializer(
+            post,
+            data=request.data,
+            partial=True  
+        )
 
-            serializer = PostCreateUpdateSerializer(
-                post,
-                data=request.data,
-                partial=True  
-            )
-
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'item': serializer.data}, status=200)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'item': serializer.data}, status=200)
             
-            return Response(serializer.errors, status=400)
-        except Exception as e:
-            return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
-    
+        return Response(serializer.errors, status=400)
+
     def delete(self, request):
         """Delete post from database"""
-        try:
-            post_id = request.query_params.get('id')
-            
-            post = Post.objects.get(id=post_id)
-            post.delete()
-            
-            return Response({'item':post}, status=HTTP_200_OK)
-        except Exception as e:
-            return Response({'error':e}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+        post_id = request.query_params.get('id')
+        
+        post = Post.objects.get(id=post_id)
+        post.delete()
+        
+        return Response({'item':post}, status=HTTP_200_OK)
