33a1c77c64dd10d6cfefb8b834fad47577b2227d
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-12-10 16:24:20 -0600

    Solved a problem of jwt bad import

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('usersList', views.UsersListView.as_view(), name='usersList'),
-    path('protectedCardsList', views.ProtectedCardsView.as_view(), name='protectedCardsList'),
     path('signUp', views.SignUpView.as_view(), name='signUp'),
     path('signIn', views.SignInView.as_view(), name='signIn'),
     path('signOut', views.SignOutView.as_view(), name='signOut'),
--- Luabla/settings.py
+++ Luabla/settings.py
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'Authentication.utils.CookieJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
